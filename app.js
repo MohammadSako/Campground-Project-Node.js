9f79669b98d167d84543ad34ff4a9b3e37d55d16
MohammadSako <71136910+MohammadSako@users.noreply.github.com>
2022-10-03 13:46:14 +0300

    Finish

--- app.js
+++ app.js
@@ -121,6 +121,7 @@ const styleSrcUrls = [
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
+    // "https://cdn.jsdelivr.net/"
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
